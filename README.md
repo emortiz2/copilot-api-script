7b582649fef268501b3069226b7f87fcf63d99e4
emortiz2 <emortiz2@squaretrade.com>
2025-01-30 09:25:22 -0800

    updated

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # copilot-api-script
 
-1. Generate Github API Key  
+1. Generate Github Access Token  
     [Setting Personal Access Token Policy for your Organization](https://docs.github.com/en/organizations/managing-programmatic-access-to-your-organization/setting-a-personal-access-token-policy-for-your-organization)  
 2. Add a .env file  
     `cd ` into this directory  
-    `echo "AUTH_TOKEN: {API KEY}" > .env`  
+    `echo "AUTH_TOKEN: {ACCESS TOKEN}" > .env`  
 3. run python or jupyter script  
--- copilot_script.py
+++ copilot_script.py
@@ -4,17 +4,34 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("AUTH_TOKEN")
+ORG = 'SquareTrade'
 
+# Headers: 
+# Accept: application/vnd.github+json
 headers = {
     "Accept": "application/vnd.github+json",
-    "Authorization": TOKEN,
+    "Authorization": f'Bearer {TOKEN}',
     "X-GitHub-Api-Version": "2022-11-28",
 }
 
-response = requests.get('https://api.github.com/orgs/SquareTrade/copilot/metrics', headers=headers)
+# path parameters:
+# org: SquareTrade **REQUIRED**
+# team_slug: name of team name
+PATH = f'https://api.github.com/orgs/{ORG}/copilot/metrics'
+
+# Query parameters:
+# since: usage metrics since this date (YYYY-MM-DD) ** max 28 days
+# until: usage metrics until this date (YYYY-MM-DD) should not preceed the since date/ go into future
+# page: page number of the results to fetch
+# per_page: number of days of metrics to display per page ** max 28 days
+params = {
+    'since': '2025-01-27',
+}
+
+response = requests.get(PATH, headers=headers, verify=False, params=params)
 
 print(response.json())
 
 outfile = './data/metrics.json'
 with open(outfile, 'w') as f:
-    f.write(response.text)
+    f.write(response.json())
